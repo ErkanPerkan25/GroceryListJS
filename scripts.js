eaa7f102f0c8f84c07bd391c04b60bed1b6a243b
Eric Hansson <erichansson.united@gmail.com>
2023-07-23 20:04:32 +0200

    Playing around with javascript to see how it works

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function helloWorld() {
+   document.getElementById("test").innerHTML = "Hello World!";
+}
+
+let item = document.getElementById("itemName");
+
+function getItem(){
+    document.getElementById("eco").innerHTML = item;
+}
